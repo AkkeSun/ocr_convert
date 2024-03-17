ec12f2763fd8690fc80d78f92216b168fcb46866
od <od@sweettracker.co.kr>
2024-03-18 00:04:45 +0900

    feat: 서비스 오타 수정 및 getTableConvertResult 반복문 수정

--- src/main/java/com/ocr_convert/service/OcrConvertServiceImpl.java
+++ src/main/java/com/ocr_convert/service/OcrConvertServiceImpl.java
@@ -16,7 +16,7 @@ public class OcrConvertServiceImpl implements OcrConvertService {
 
             double[] y = {0, 0, 0, 0};
             int sameCnt = 0;
-            StringBuilder convetString = new StringBuilder();
+            StringBuilder convertString = new StringBuilder();
 
             for (int i = 0; i < fields.length(); i++) {
                 JSONObject field = fields.getJSONObject(i);
@@ -31,7 +31,7 @@ public class OcrConvertServiceImpl implements OcrConvertService {
                         y[1] = vertices.getJSONObject(1).getDouble("y");
                         y[2] = vertices.getJSONObject(2).getDouble("y");
                         y[3] = vertices.getJSONObject(3).getDouble("y");
-                        convetString.append(inferText).append(" ");
+                        convertString.append(inferText).append(" ");
                         continue;
                     }
                     for (int j = 0; j < vertices.length(); j++) {
@@ -42,16 +42,16 @@ public class OcrConvertServiceImpl implements OcrConvertService {
                     }
 
                     if (sameCnt >= 2) {
-                        convetString.append(inferText).append(" ");
+                        convertString.append(inferText).append(" ");
                     } else {
-                        convetString.append("\n").append(inferText).append(" ");
+                        convertString.append("\n").append(inferText).append(" ");
                     }
 
                     sameCnt = 0;
                 }
             }
 
-            return convetString.toString();
+            return convertString.toString();
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
@@ -64,26 +64,33 @@ public class OcrConvertServiceImpl implements OcrConvertService {
         try {
             JSONObject json = new JSONObject(payload);
             JSONArray tables = json.getJSONArray("images").getJSONObject(0).getJSONArray("tables");
-            StringBuilder convetString = new StringBuilder();
+            StringBuilder convertString = new StringBuilder();
 
-            for (int k = 0; k < tables.length(); k++) {
+            for (int i = 0; i < tables.length(); i++) {
 
-                JSONArray cells = tables.getJSONObject(k).getJSONArray("cells");
-                for (int i = 0; i < cells.length(); i++) {
-                    JSONArray cellTextLines = cells.getJSONObject(i).getJSONArray("cellTextLines");
+                JSONArray cells = tables.getJSONObject(i).getJSONArray("cells");
+                for (int j = 0; j < cells.length(); j++) {
+                    JSONArray cellTextLines = cells.getJSONObject(j).getJSONArray("cellTextLines");
                     if (cellTextLines.length() == 0) {
                         continue;
                     }
 
-                    JSONArray cellWords = cellTextLines.getJSONObject(0).getJSONArray("cellWords");
-                    for (int j = 0; j < cellWords.length(); j++) {
-                        convetString.append(cellWords.getJSONObject(j).getString("inferText"))
-                            .append(" ");
+                    for (int k = 0; k < cellTextLines.length(); k++) {
+                        JSONArray cellWords = cellTextLines.getJSONObject(k)
+                            .getJSONArray("cellWords");
+                        for (int x = 0; x < cellWords.length(); x++) {
+                            convertString.append(cellWords.getJSONObject(x).getString("inferText"))
+                                .append(" ");
+                        }
+                        convertString.append("\n");
                     }
-                    convetString.append("\n");
+
+                    convertString.append("\n")
+                        .append("----------------------------")
+                        .append("\n");
                 }
             }
-            return convetString.toString();
+            return convertString.toString();
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
